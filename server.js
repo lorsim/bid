ddbf017befe84bc1793816727df77b94c9a9e621
lorsim <lorsim96@gmail.com>
2021-04-24 15:12:13 +0530

    error handling

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import bidRoutes from './routes/bidRoutes.js'
 
 dotenv.config()
@@ -9,6 +10,9 @@ const app =express()
 
 app.use(express.json())
 app.use('/api/bid', bidRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`))
